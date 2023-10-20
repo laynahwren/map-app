52df1ccdf8090909eeb819795f682cd381855d14
Laynah Varnum <laynah.varnum98@gmail.com>
2023-10-20 16:54:07 -0700

    get data popup to appear

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -11,23 +11,59 @@ import './Map.css'
 
 
 const Map = (props) => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoibGF5bmFod3JlbiIsImEiOiJjbG55eTlpaWwwMm0wMnNyc3dnOTZ3MGVtIn0.NZjocPvkrGFUYNFfhGNKOA'
-    const mapContainer = useRef(null)
-    const map = useRef(null)
-  
-    useEffect(() => {
-      if (map.current) return
-      map.current = new mapboxgl.Map({
-        container: mapContainer.current,
-        style: 'mapbox://styles/laynahwren/clnyyugvw004601q222r42b3b',
-        center: [-97.5, 38.5], // starting position [lng, lat]
-        zoom: 3.3 // starting zoom
-      });
+  mapboxgl.accessToken = 'pk.eyJ1IjoibGF5bmFod3JlbiIsImEiOiJjbG55eTlpaWwwMm0wMnNyc3dnOTZ3MGVtIn0.NZjocPvkrGFUYNFfhGNKOA'
+  const mapContainer = useRef(null)
+  const map = useRef(null)
+
+  useEffect(() => {
+    if (map.current) return
+    const mapBox = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/laynahwren/clnyyugvw004601q222r42b3b',
+      center: [-97.5, 41], // starting position [lng, lat]
+      zoom: 2.6, // starting zoom
+      minZoom: 2.6
+    });
+
+    map.current = mapBox
+
+    mapBox.on('load', () => {
+      mapBox.addLayer({
+        id: 'data-layer',
+        type: 'fill',
+        source: {
+          type: 'vector',
+          url: 'mapbox://laynahwren.cgjgscwc'
+        },
+        'source-layer': 'National_Obesity_By_State-2ay1vb',
+        paint: {
+          'fill-color': 'transparent'
+        }
+      })
     })
 
-    return (
-      <div id='map' ref={mapContainer} />
-    );
+    mapBox.on('click', 'data-layer', (e) => {
+      console.log(e)
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(`State: ${e.features[0].properties.NAME}
+        Obesity: ${e.features[0].properties.Obesity}`)
+        .addTo(mapBox);
+    })
+
+    mapBox.on('mouseenter', 'data-layer', () => {
+      mapBox.getCanvas().style.cursor = 'pointer';
+    });
+
+    mapBox.on('mouseleave', 'data-layer', () => {
+      mapBox.getCanvas().style.cursor = '';
+    })
+
+  })
+
+  return (
+    <div id='map' ref={mapContainer} />
+  );
 }
 
 export default Map
