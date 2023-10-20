37fb68710c982cb15736e03c198566182fdb102a
Laynah Varnum <laynah.varnum98@gmail.com>
2023-10-20 13:13:25 -0700

    add basic map component

---
+++ client/src/components/Map.tsx
@@ -0,0 +1,33 @@
+import { useRef, useEffect } from 'react'
+import 'mapbox-gl/dist/mapbox-gl.css'
+import mapboxgl from 'mapbox-gl'
+import '../App.css'
+
+// declare var process: {
+//     env: {
+//         REACT_APP_MAPBOX_TOKEN: string
+//     }
+// }
+
+
+const Map = () => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibGF5bmFod3JlbiIsImEiOiJjbG55eTlpaWwwMm0wMnNyc3dnOTZ3MGVtIn0.NZjocPvkrGFUYNFfhGNKOA'
+    const mapContainer = useRef(null)
+    const map = useRef(null)
+  
+    useEffect(() => {
+      if (map.current) return
+      map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/laynahwren/clnyyugvw004601q222r42b3b',
+        center: [-97.5, 38.5], // starting position [lng, lat]
+        zoom: 3.3 // starting zoom
+      });
+    })
+
+    return (
+      <div id='map' ref={mapContainer} />
+    );
+}
+
+export default Map
